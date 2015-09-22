daa91116c6c6790a9cf3082e9759f67c9afe2f28
Gabriel Ehrlich <gabriel.s.ehrlich@gmail.com>
2015-09-22 17:33:01 +0200

    Re-hardcoded newton as cam_controls camera so that it's easier to change

--- code/cam_full/cam_full.py
+++ code/cam_full/cam_full.py
@@ -1,38 +1,38 @@
 import cam_control, acq_control, rms, ratio
-from andor.andorcamera import newton
+from andor.andorcamera import newton, idus
 from PyQt4 import QtGui as gui, QtCore as core
 from plotter import PlotterWidget, AvgPlotter
 
-import time
+cam = newton
 
 def start_the_rest():
     global start_w, rms_w, ratio_w, probe_only_w, pump_probe_w
     reload(acq_control)
     reload(rms)
     reload(ratio)
-    start_w = acq_control.SimpleControllerWidget(newton)
+    start_w = acq_control.SimpleControllerWidget(cam)
     start_w.show()
 
     rms_w = PlotterWidget(rms.RmsPlotter, "RMS/mean counts",
-        newton.x, start_w.new_probe_only)
+        cam.x, start_w.new_probe_only)
     rms_w.show()
     start_w.startDisplay.connect(rms_w.startAcq)
     start_w.abortDisplay.connect(rms_w.abortAcq)
 
     ratio_w = PlotterWidget(ratio.RatioPlotter, "Probe-only/pump-probe counts",
-        newton.x, start_w.new_probe_only, start_w.new_pump_probe)
+        cam.x, start_w.new_probe_only, start_w.new_pump_probe)
     ratio_w.show()
     start_w.startDisplay.connect(ratio_w.startAcq)
     start_w.abortDisplay.connect(ratio_w.abortAcq)
 
     probe_only_w = PlotterWidget(AvgPlotter, "Probe only",
-        newton.x, start_w.new_probe_only)
+        cam.x, start_w.new_probe_only)
     probe_only_w.show()
     start_w.startDisplay.connect(probe_only_w.startAcq)
     start_w.abortDisplay.connect(probe_only_w.abortAcq)
 
     pump_probe_w = PlotterWidget(AvgPlotter, "Pump-probe",
-        newton.x, start_w.new_pump_probe)
+        cam.x, start_w.new_pump_probe)
     pump_probe_w.show()
     start_w.startDisplay.connect(pump_probe_w.startAcq)
     start_w.abortDisplay.connect(pump_probe_w.abortAcq)
@@ -45,7 +45,7 @@ def close_the_rest():
     pump_probe_w.close()
 
 app = gui.QApplication([])
-init_w = cam_control.CameraControlWidget(newton)
+init_w = cam_control.CameraControlWidget(cam)
 init_w.show()
 init_w.cam_initialize_done.connect(start_the_rest)
 init_w.cam_shut_down.connect(close_the_rest)
